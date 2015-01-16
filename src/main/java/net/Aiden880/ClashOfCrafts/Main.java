1607f3da76b3be417495dc330ed5b25b3b7f2298
Aiden880 <Aiden.Hassall1@gmail.com>
2015-01-16 14:38:10 +0000

    Corrected indentation

--- src/main/java/net/Aiden880/ClashOfCrafts/Main.java
+++ src/main/java/net/Aiden880/ClashOfCrafts/Main.java
@@ -11,8 +11,8 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		getCommand("coc").setExecutor( new CocCommand() );
 		getCommand("user").setExecutor( new UserCommand() );
-        getServer().getPluginManager().registerEvents(new DecayListener(), this);	
-        getServer().getPluginManager().registerEvents(new NewUserListener(), this);		
+		getServer().getPluginManager().registerEvents(new DecayListener(), this);	
+        	getServer().getPluginManager().registerEvents(new NewUserListener(), this);		
 	}
 	public void onDisable() {
 		
